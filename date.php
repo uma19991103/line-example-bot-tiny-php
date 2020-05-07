de4595fb759c57520f8b1ca8c54f897643407a22
Asthmae <uma0908677205@gmail.com>
2020-05-07 15:26:23 +0800

    add remote html address

---
+++ date.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to my heroku</h1>
+    <p>0507 class usage for line api</p>
+    <p>Today is <?php echo date("m/d/y"); ?> (M/D/Y) </p>
+</body>
+</html>
